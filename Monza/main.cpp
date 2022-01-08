f3a1172aa8f49a5484d8342adc2b7f1c59a68c35
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2022-01-08 08:35:43 +0100

    Initial Commit

---
+++ Monza/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Monza
+//
+//  Created by Ziyad Mourabiti on 8/1/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
