1840d1ad31c52d8f616332de98fce719f023575a
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2022-01-21 15:13:27 +0100

    feat: add bit count and ls1b macros, mapping of square coords.

--- Monza/constants.hpp
+++ Monza/constants.hpp
@@ -34,7 +34,7 @@ const Bitboard notABFile = 18229723555195321596ULL;
 const Bitboard notHFile  = 9187201950435737471ULL; // 0x7f7f7f7f7f7f7f7f; // ~0x8080808080808080
 const Bitboard notHGFile = 4557430888798830399ULL;
 
-/*
+const char * squares[64] = {
  "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
  "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
  "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
@@ -42,8 +42,8 @@ const Bitboard notHGFile = 4557430888798830399ULL;
  "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
  "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
  "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
- "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
- */
+ "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1" };
+ 
 
 /*
  for (int i=8; i>=1; i--)
@@ -52,8 +52,24 @@ const Bitboard notHGFile = 4557430888798830399ULL;
 
 // Bitboard Macros
 
+// # define pop_bit(board, square) (bit_on_square(board, square) ? (board) ^= (1ULL << square) : 0)
 # define bit_on_square(board, square) ((board) & (1ULL << square))
 # define set_bit(board, square) ((board) |= (1ULL << square))
-//# define pop_bit(board, square) (bit_on_square(board, square) ? (board) ^= (1ULL << square) : 0)
 # define pop_bit(board, square) ((board) &= ~(1ULL << (square)))
+# define count_bits(bitboard) __builtin_popcountll(bitboard)
+# define ls1b(bitboard) (__builtin_ctzll(bitboard))
+// Returns the number of trailing 0-bits in x, starting at the least significant bit position. If x is 0, the result is undefined. https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html
+
+
+
+// Brian Kernighan’s Algorithm - https://www.geeksforgeeks.org/count-set-bits-in-an-integer/
+//
+// static inline int count_bits(Bitboard bitboard) {
+//    int count = 0;
+//    while (bitboard) {
+//        count++;
+//        bitboard &= bitboard - 1;
+//    }
+//    return count;
+// }
 
