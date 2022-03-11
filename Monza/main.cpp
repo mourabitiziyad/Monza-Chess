ab677348b98884eb9e9b850b60401241ce9366c3
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2022-03-11 03:10:24 +0100

    bug fixes, move parsing, UCI

--- Monza/main.cpp
+++ Monza/main.cpp
@@ -443,11 +443,11 @@ void parse_fen(char * fen_string) {
     for (int square = 0; square < 64 && *fen_string && *fen_string!=' ';) {
         if (isalpha(*fen_string)) set_bit(piece_bitboards[char_pieces[*fen_string++]], square++);
         else if (isdigit(*fen_string)) square += (*fen_string++ - '0');
-        else if (*fen_string == '/') *fen_string++;
+        else if (*fen_string == '/') fen_string++;
         else memset(piece_bitboards, 0ULL, sizeof(piece_bitboards));
     }
     *(++fen_string) == 'b' ? (turn = black) : (turn = white);
-    *(fen_string += 2);
+    (fen_string += 2);
     while (*fen_string != ' ') {
         switch (*fen_string++) {
             case 'K': castling_rights |= wk; break;
@@ -912,45 +912,239 @@ void init() {
     
 }
 
-//      +---+---+---+---+---+---+---+---+---+---+
-//      +---+---+---+---+ main  +---+---+---+---+
-//      +---+---+---+---+---+---+---+---+---+---+
+long long nodes;
+
+static inline void perft_driver(int depth) {
+    if (depth == 0) {
+        nodes++;
+        return;
+    }
+    
+    moves move_list[1];
+    generate_all_moves(move_list);
+    
+    for (int moves = 0; moves < move_list->count; moves++) {
+        copy();
+        if (!make_move(move_list->moves[moves], all)) continue;
+        perft_driver(depth - 1);
+        take_back();    
+//        display_UCI_move(move_list->moves[moves]);
+    }
+}
+
+void perft_test(int depth) {
+    
+    moves move_list[1];
+    generate_all_moves(move_list);
+    
+    auto startTime = chrono::high_resolution_clock::now();
+    
+    for (int moves = 0; moves < move_list->count; moves++) {
+        copy();
+        if (!make_move(move_list->moves[moves], all)) continue;
+        long cummulative_nodes = nodes;
+        perft_driver(depth - 1);
+        long old_nodes = nodes - cummulative_nodes;
+        take_back();
+        printf("     move: %s%s%c  nodes: %ld\n", squares[get_move_source(move_list->moves[moves])],
+                                                        squares[get_move_target(move_list->moves[moves])],
+                                                        get_move_promoted(move_list->moves[moves]) ? promoted_pieces[get_move_promoted(move_list->moves[moves])] : ' ',
+                                                        old_nodes);
+    }
+    auto endTime = chrono::high_resolution_clock::now();
+    printf("\n     Depth: %d\n", depth);
+    printf("     Nodes: %lld\n", nodes);
+    cout << "     Time: " << chrono::duration_cast < chrono::milliseconds > (endTime - startTime).count() << "ms" << endl;
+}
+
+int parse_move(char *move_string) {
+    moves move_list[1];
+    generate_all_moves(move_list);
+    int source_square = (move_string[0] - 'a') + (8 - (move_string[1] - '0')) * 8;
+    int target_square = (move_string[2] - 'a') + (8 - (move_string[3] - '0')) * 8;
+    for (int move_count = 0; move_count < move_list->count; move_count++)
+    {
+        int move = move_list->moves[move_count];
+        if (source_square == get_move_source(move) && target_square == get_move_target(move))
+        {
+            int promoted_piece = get_move_promoted(move);
+            if (promoted_piece)
+            {
+                if ((promoted_piece == Q || promoted_piece == q) && move_string[4] == 'q')
+                    return move;
+                else if ((promoted_piece == R || promoted_piece == r) && move_string[4] == 'r')
+                    return move;
+                else if ((promoted_piece == B || promoted_piece == b) && move_string[4] == 'b')
+                    return move;
+                else if ((promoted_piece == N || promoted_piece == n) && move_string[4] == 'n')
+                    return move;
+                continue;
+            }
+            
+            // return legal move
+            return move;
+        }
+    }
+    // return illegal move
+    return 0;
+}
+
+void search_position(int depth) {
+    printf("bestmove: d2d4\n");
+}
+
+/*
+    Example UCI commands to init position on chess board
+    
+    // init start position
+    position startpos
+    
+    // init start position and make the moves on chess board
+    position startpos moves e2e4 e7e5
+    
+    // init position from FEN string
+    position fen r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1
+    
+    // init position from fen string and make moves on chess board
+    position fen r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 moves e2a6 e8g8
+*/
+
+// parse UCI "position" command
+void parse_position(char *command) // taken from BBC.c (Code Monkey King)
+{
+    // shift pointer to the right where next token begins
+    command += 9;
+    
+    // init pointer to the current character in the command string
+    char *current_char = command;
+    
+    // parse UCI "startpos" command
+    if (!strncmp(command, "startpos", 8))
+        // init chess board with start position
+        parse_fen((char *) starting_position);
+    
+    // parse UCI "fen" command
+    else
+    {
+        // make sure "fen" command is available within command string
+        current_char = strstr(command, "fen");
+        
+        // if no "fen" command is available within command string
+        if (current_char == NULL)
+            // init chess board with start position
+            parse_fen((char *)starting_position);
+            
+        // found "fen" substring
+        else
+        {
+            // shift pointer to the right where next token begins
+            current_char += 4;
+            
+            // init chess board with position from FEN string
+            parse_fen(current_char);
+        }
+    }
+    
+    // parse moves after position
+    current_char = strstr(command, "moves");
+    
+    // moves available
+    if (current_char != NULL)
+    {
+        // shift pointer to the right where next token begins
+        current_char += 6;
+        
+        // loop over moves within a move string
+        while(*current_char)
+        {
+            // parse next move
+            int move = parse_move(current_char);
+            
+            // if no more moves
+            if (move == 0)
+                // break out of the loop
+                break;
+            
+            // make move on the chess board
+            make_move(move, all);
+            
+            // move current character mointer to the end of current move
+            while (*current_char && *current_char != ' ') current_char++;
+            
+            // go to the next move
+            current_char++;
+        }
+    }
+    styled_board();
+}
+
+void parse_go(char * command) {
+    int depth = -1;
+    char * current_depth = NULL;
+    if (current_depth = strstr(command, "depth")) {
+        depth = atoi(current_depth + 6);
+    } else depth = 6;
+    
+    search_position(depth);
+    // call search pos given depth
+}
+
+void UCI_loop() {
+    setbuf(stdin, NULL);
+    setbuf(stdout, NULL);
+    
+    char input[3000];
+    
+    printf("id name Monza\n");
+    printf("id author Ziyad Mourabiti\n");
+    printf("uciok\n"); // chess engine speaks UCI
+    
+    while (1) {
+        memset(input, 0, sizeof(input));
+        fflush(stdout);
+        if (!fgets(input, 3000, stdin) || input[0] == '\n') continue;
+        
+        else
+            if (!strncmp(input, "isready", 7)) {
+            cout << "readyok" << endl;
+            continue;
+        }
+        else if (!strncmp(input, "position", 8)) {
+            parse_position(input);
+        }
+        else if (!strncmp(input, "ucinewgame", 10)) {
+            parse_position("position startpos");
+        }
+        else if (!strncmp(input, "go", 2)) {
+            parse_go(input);
+        }
+        else if (!strncmp(input, "quit", 4)) break;
+        else if (!strncmp(input, "uci", 3)) {
+            printf("id name Monza\n");
+            printf("id author Ziyad Mourabiti\n");
+            printf("uciok\n"); // chess engine speaks UCI
+        }
+    }
+}
 
 int main(int argc, const char * argv[]) {
     
     init();
+    UCI_loop();
+//    parse_fen((char *)r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1);
+//    parse_position((c har *)"position startpos moves e2e4 e7e5");
+//    parse_go("go depth 6");
+//    styled_board();
 
-    parse_fen((char *)"r3k2r/p1ppqpb1/1n2pnp1/3PN3/1p2P3/2N2Q1p/PPPBqPPP/R3K2R w KQkq - 0 1");
-//    parse_fen((char *) tricky_position);
-    styled_board();
+//    int move = parse_move((char *)"g2g1q");
+//    if (move) {
+//        make_move(move, all);
+//        styled_board();
+//    } else {
+//        printf("illegal\n");
+//    }
     
-    
-    auto startTime = chrono::high_resolution_clock::now();
-
-    moves move_list[1];
-    generate_all_moves(move_list);
-    display_move_list(move_list);
-    
-
-    
-
-    for (int moves = 0; moves < move_list->count; moves++) {
-        int move = move_list->moves[moves];
-        copy();
-        if (!make_move(move, all)) continue;
-        styled_board();
-        getchar();
-
-        take_back();
-        styled_board();
-        getchar();
-    }
-    
-    auto endTime = chrono::high_resolution_clock::now();
-
-    cout << "Milliseconds: " << chrono::duration_cast < chrono::milliseconds > (endTime - startTime).count() << endl;
-
-    
+//    perft_test(7);
     
     return 0;
 }
